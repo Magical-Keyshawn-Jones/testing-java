906dbebddf55f8a798b0b5d0a3c9e7bfe6b3cafc
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-11-26 15:18:52 -0600

    variable manipulation

---
+++ src/VariableManipulation.java
@@ -0,0 +1,11 @@
+public class VariableManipulation {
+    public static void main(String[] args) {
+        int age = 16;
+        int theirAge = 16;
+        int year = 2022;
+        String name1 = "Kiwi";
+        String name2 = "Kiwi";
+        int birthYear = year - age;
+        System.out.println(name1.equals(name2));
+    }
+}
--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,10 @@
 public class Variables {
     public static void main(String[] args) {
       System.out.print("Testing output");  
+      int age = 16;
+      double decimals = 8.445;
+      char singleLetter = 'A';
+      String words = "I love cheese";
+      String wordsWithQuotes = "\"I hate cheese\"";
     }
 }
